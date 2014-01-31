ead4bc9e49bbd37006c993ed759b92e30c49ffca
io <io@ios-MacBook-Air.local>
2014-01-30 22:54:54 -0800

    crude crud demo

--- partial/header/header.js
+++ partial/header/header.js
@@ -1,5 +1,20 @@
-angular.module('ecopos').controller('HeaderCtrl',function($scope, navigation){
+angular.module('ecopos').controller('HeaderCtrl', function($scope, navigation, $firebase, Firebase){
 
-$scope.links = navigation;
+    $scope.links = navigation;
+    $scope.items = $firebase(new Firebase('https://ecopos.firebaseio.com/items'));
+
+    $scope.addItem = function(){
+        $scope.items.$add({test:"run"});
+    };
+
+    $scope.loseItem = function(){
+        $scope.items.$remove("test");
+    };
+
+    $scope.loseAllItems = function(){
+        $scope.items.$remove();
+    };
+
+
 
 });
