0916b8765c7dfdaf28e9bdd648c75d6d5e620cc2
Pat Long <plong00@gmail.com>
2014-02-05 13:59:34 -0800

    notify service

--- modules/ecoposcommon/service/notify.js
+++ modules/ecoposcommon/service/notify.js
@@ -1,6 +1,11 @@
-angular.module('ecopos.common').factory('notify',function() {
+angular.module('ecopos.common').factory('notify',function($rootScope) {
 
-	var notify = {};
+	var notify = {
+    addNote: function(uid, note){
+      var notes = $rootScope.DBFB.$child('notes');
+      notes.$child(uid).$add({'note': note});
+    }
+  };
 
 	return notify;
 });
