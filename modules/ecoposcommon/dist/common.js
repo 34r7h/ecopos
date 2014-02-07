5293ab0b34e1494e55fb5e151a3e693a7af2ea90
Pat Long <plong00@gmail.com>
2014-02-06 18:30:04 -0800

    linking user profile to auth-provider accounts

--- modules/ecoposcommon/dist/common.js
+++ modules/ecoposcommon/dist/common.js
@@ -17,9 +17,233 @@ angular.module('ecopos.common').factory('notify',function() {
 
 	return notify;
 });
-angular.module('ecopos.common').factory('authority',function() {
+angular.module('ecopos.common').factory('authority',function($rootScope, $firebaseSimpleLogin, $q) {
+  var auth = $firebaseSimpleLogin($rootScope.DBFBref);
 
-	var authority = {};
+	var authority = {
+    getUserByUID: function(uid){
+      var d = $q.defer();
+
+      // first, try to load the linkedAccount by its uid
+      $rootScope.DBFBref.child('linkedAccount/'+uid).
+          once('value',
+          function(linkedSnap){
+            var linkedVal = linkedSnap.val();
+            if(linkedVal){
+              // we found the linkedAccount record for uid
+              $rootScope.DBFBref.child('user/'+linkedVal.userID).
+                  once('value',
+                  function(userSnap){
+                    d.resolve(userSnap.val());
+                  },
+                  function(err){
+                    d.reject('error loading user profile:'+err);
+                  });
+            }
+            else{
+              // could not load the linkedAccount
+              d.resolve(null);
+            }
+          },
+          function(err){
+            d.reject('error loading linked:'+err);
+          }
+      );
+      //"simplelogin:3","github:584954","facebook:304703258"
+
+      return d.promise;
+    },
+    saveUserData: function(data){
+      // to avoid accidentally duplicating a user, data.userID must be explicitly set
+      if(data.userID === undefined){ return; } // for a new user, use data.userID = null
+
+      var dbUserRef = $rootScope.DBFBref.child('user');
+
+      // create a new user profile if no userID given
+      if(data.userID === null){ data.userID = dbUserRef.push().name(); }
+
+      // handle linking with accounts
+      if(data.uid){
+        if(!(data.uid instanceof Array)){ data.uid = [data.uid]; } // make it an array
+
+        var linkBools = {};
+
+        // handle every uid we've been told to link with
+        $.each(data.uid, function(key, uid){
+          // break apart the uid to discover its provider and id
+          var uidParts = uid.split(':', 2);
+          var cProv = 'unknown';
+          var cId = 0;
+          if(uidParts.length === 2){
+            cProv = uidParts[0];
+            cId = uidParts[1];
+          }
+
+          // associate this account with userID
+          var cLinkAccount = $rootScope.DBFBref.child('linkedAccount/'+uid);
+          cLinkAccount.once('value',
+              function(snap){
+                var cLinkAccountData = snap.val();
+                if(!cLinkAccountData){
+                  // linkedAccount is not setup
+                  cLinkAccount.set({userID: data.userID, provider: cProv, id: cId});
+                  linkBools[uid] = true; // we'd prefer to have the uid as the index rather than value (more efficient lookups)
+                }
+                else{
+                  // linkedAccount is there...
+                  // TODO: check the userID
+                  //   if it is different from data.userID
+                  //     then this account is already linked to a different profile (throw an error?)
+                  console.log('what is linkedAccount:'+JSON.stringify(cLinkAccountData));
+                }
+              },
+              function(err){
+                console.log('error:'+err);
+              }
+          );
+        });
+
+        // ok, we can update the user profile
+        dbUserRef.child(data.userID).
+            update({displayName: data.name, testing: 'testing 123...', linkedAccounts: linkBools, created: new Date(), lastLogin: new Date()});
+      }
+    },
+    createUser: function(email, password, callback){
+      auth.$createUser(email, password, true).then(
+          function(user){
+            if(callback){
+              callback(null, user);
+            }
+          },
+          function(error){
+            if(callback){
+              callback(error, null);
+            }
+          }
+      );
+    },
+    authUser: function(email, password, callback){
+      auth.$login('password', {email: email, password: password}).then(
+          function(user){
+            if(callback){
+              callback(null, user);
+            }
+          },
+          function(error){
+            if(callback){
+              callback(error, null);
+            }
+          }
+      );
+    },
+    authFacebook: function(callback){
+      auth.$login('facebook').then(
+          function(user){
+            if(callback){
+              callback(null, user);
+            }
+          },
+          function(error){
+            if(callback){
+              callback(error, null);
+            }
+          }
+      );
+    },
+    authTwitter: function(callback){
+      auth.$login('twitter').then(
+          function(user){
+            if(callback){
+              callback(null, user);
+            }
+          },
+          function(error){
+            if(callback){
+              callback(error, null);
+            }
+          }
+      );
+    },
+    authGitHub: function(callback){
+      auth.$login('github').then(
+          function(user){
+            if(callback){
+              callback(null, user);
+            }
+          },
+          function(error){
+            if(callback){
+              callback(error, null);
+            }
+          }
+      );
+    },
+    logout: function(){
+      auth.$logout();
+    },
+    getUserData: function(){
+      var userData = {};
+
+      if(auth.user && auth.user.id){
+        userData = {
+          provider: auth.user.provider,
+          id: auth.user.id,
+          uid: auth.user.uid,
+          authToken: auth.user.firebaseAuthToken
+        };
+
+        if(auth.user.provider === 'password'){
+          userData.loginService = 'User Account';
+          userData.username = auth.user.email;
+          userData.email = auth.user.email;
+          userData.displayName = auth.user.email;
+        }
+        else if(auth.user.provider === 'facebook'){
+          userData.loginService = 'Facebook';
+          userData.username = auth.user.username;
+          userData.email = auth.user.email;
+          userData.displayName = auth.user.displayName;
+        }
+        else if(auth.user.provider === 'twitter'){
+          userData.loginService = 'Twitter';
+          userData.username = auth.user.username;
+          userData.email = auth.user.email;
+          userData.displayName = auth.user.displayName;
+        }
+        else if(auth.user.provider === 'github'){
+          userData.loginService = 'GitHub';
+          userData.username = auth.user.username;
+          userData.email = auth.user.email;
+          userData.displayName = (auth.user.displayName?auth.user.displayName:auth.user.username);
+        }
+
+        if(userData.uid){
+          userData.notes = $rootScope.DBFB.$child('notes/'+userData.uid);
+          var userProfile = null;
+          authority.getUserByUID(userData.uid).
+              then(
+                function(userProfile){
+                  if(userProfile){
+                    console.log('win:'+JSON.stringify(userProfile));
+                    userData.userID = userProfile.userID;
+                    $rootScope.DBFB.$child('user/'+userData.userID+'/lastLogin').
+                        $set(new Date());
+                  }
+                  else{
+                    console.log('creating new user profile for uid:'+userData.uid);
+                    authority.saveUserData({userID: null, uid: userData.uid, name: userData.displayName, lastLogin: new Date()});
+                  }
+                },
+                function(err){
+                  console.log('lose:'+err);
+                }
+              );
+        }
+      }
+
+      return userData;
+    }
+  };
 
 	return authority;
 });
@@ -62,16 +286,109 @@ angular.module('ecopos.common').directive('sharing', function() {
 	};
 });
 
-angular.module('ecopos.common').directive('login', function() {
+angular.module('ecopos.common').directive('login', function(authority, $rootScope) {
 	return {
 		restrict: 'E',
 		replace: true,
-		scope: {
-
-		},
 		templateUrl: 'directive/login/login.html',
 		link: function(scope, element, attrs, fn) {
+      $rootScope.user = null;
+      $rootScope.err = 'no errors.';
 
+      scope.email = '';
+      scope.password = '';
+
+
+      $rootScope.$on('$firebaseSimpleLogin:login', function(event){
+        $rootScope.user = authority.getUserData();
+      });
+      $rootScope.$on('$firebaseSimpleLogin:logout', function(event){
+        $rootScope.user = authority.getUserData();
+      });
+
+      scope.addUser = function(){
+        if( !scope.email ) {
+          $rootScope.err = 'Please enter an email address';
+        }
+        else if( !scope.password ) {
+          $rootScope.err = 'Please enter a password';
+        }
+        else{
+          authority.createUser(scope.email, scope.password, function(err, user){
+            if(err){
+              $rootScope.err = err.message;
+            }
+            else if(user){
+              // TODO: visual notify of user creation
+              console.log('created user:'+user.id);
+            }
+            else{
+              $rootScope.err = 'Unknown error (Add user)';
+            }
+
+          });
+        }
+      };
+
+      scope.login = function(){
+        if( !scope.email ) {
+          $rootScope.err = 'Please enter an email address';
+        }
+        else if( !scope.password ) {
+          $rootScope.err = 'Please enter a password';
+        }
+        else{
+          authority.authUser(scope.email, scope.password, function(err, user){
+            if(err){
+              $rootScope.err = err.message;
+            }
+            else if(user){}
+            else{
+              $rootScope.err = 'Unknown error (Authentication)';
+            }
+          });
+        }
+      };
+
+      scope.logout = function(){
+        authority.logout();
+      };
+
+      scope.loginFacebook = function(){
+        authority.authFacebook(function(err, user){
+          if(err){
+            $rootScope.err = err.message;
+          }
+          else if(user){}
+          else{
+            $rootScope.err = 'Unknown error (Facebook)';
+          }
+        });
+      };
+
+      scope.loginTwitter = function(){
+        authority.authTwitter(function(err, user){
+          if(err){
+            $rootScope.err = err.message;
+          }
+          else if(user){}
+          else{
+            $rootScope.err = 'Unknown error (Twitter)';
+          }
+        });
+      };
+
+      scope.loginGitHub = function(){
+        authority.authGitHub(function(err, user){
+          if(err){
+            $rootScope.err = err.message;
+          }
+          else if(user){}
+          else{
+            $rootScope.err = 'Unknown error (GitHub)';
+          }
+        });
+      };
 
 		}
 	};
@@ -96,7 +413,7 @@ angular.module('ecopos.common').run(['$templateCache', function($templateCache) 
   'use strict';
 
   $templateCache.put('directive/login/login.html',
-    "<div></div>"
+    "<div><div class=user-block data-ng-show=user.uid><span data-ng-show=user.displayName>Logged in as: {{ user.displayName }}<span data-ng-show=user.loginService>&nbsp;via {{ user.loginService }}</span><span data-ng-show=user.id>&nbsp;(#{{ user.id }})</span></span><div><input type=button value=Logout data-ng-click=logout()></div></div><div class=profile-block data-ng-show=user.uid><h3>My Profile</h3><h4>{{ user.displayName }}</h4><div><label for=displayName>Name:</label><input name=displayName id=displayName data-ng-model=user.displayName></div><div><input type=button value=\"Save Profile\" data-ng-click=saveUserData()></div></div><div class=anonymous-block data-ng-hide=user.uid><div class=register-block><h3>Register</h3><div><label for=email>Email:</label><input name=email id=email data-ng-model=email></div><div><label for=password>Password:</label><input type=password name=password id=password data-ng-model=password></div><div><label for=passwordConfirm>Confirm password:</label><input type=password name=passwordConfirm id=passwordConfirm data-ng-model=passwordConfirm></div><div><input type=button value=\"Create User\" data-ng-click=addUser()></div></div><div class=login-block><h3>Login</h3><div><label for=email>Email:</label><input name=email id=email data-ng-model=email></div><div><label for=password>Password:</label><input type=password name=password id=password data-ng-model=password></div><div><input type=button value=Login data-ng-click=login()></div><div><input type=button value=\"Login with Facebook\" data-ng-click=loginFacebook()></div><div><input type=button value=\"Login with Twitter\" data-ng-click=loginTwitter()></div><div><input type=button value=\"Login with GitHub\" data-ng-click=loginGitHub()></div></div></div></div>"
   );
 
 
--- modules/ecoposcommon/service/authority.js
+++ modules/ecoposcommon/service/authority.js
@@ -1,9 +1,95 @@
-angular.module('ecopos.common').factory('authority',function($rootScope, $firebaseSimpleLogin) {
+angular.module('ecopos.common').factory('authority',function($rootScope, $firebaseSimpleLogin, $q) {
   var auth = $firebaseSimpleLogin($rootScope.DBFBref);
 
 	var authority = {
-    createUser: function(email, password, callback){
+    getUserByUID: function(uid){
+      var d = $q.defer();
 
+      // first, try to load the linkedAccount by its uid
+      $rootScope.DBFBref.child('linkedAccount/'+uid).
+          once('value',
+          function(linkedSnap){
+            var linkedVal = linkedSnap.val();
+            if(linkedVal){
+              // we found the linkedAccount record for uid
+              $rootScope.DBFBref.child('user/'+linkedVal.userID).
+                  once('value',
+                  function(userSnap){
+                    d.resolve(userSnap.val());
+                  },
+                  function(err){
+                    d.reject('error loading user profile:'+err);
+                  });
+            }
+            else{
+              // could not load the linkedAccount
+              d.resolve(null);
+            }
+          },
+          function(err){
+            d.reject('error loading linked:'+err);
+          }
+      );
+      //"simplelogin:3","github:584954","facebook:304703258"
+
+      return d.promise;
+    },
+    saveUserData: function(data){
+      // to avoid accidentally duplicating a user, data.userID must be explicitly set
+      if(data.userID === undefined){ return; } // for a new user, use data.userID = null
+
+      var dbUserRef = $rootScope.DBFBref.child('user');
+
+      // create a new user profile if no userID given
+      if(data.userID === null){ data.userID = dbUserRef.push().name(); }
+
+      // handle linking with accounts
+      if(data.uid){
+        if(!(data.uid instanceof Array)){ data.uid = [data.uid]; } // make it an array
+
+        var linkBools = {};
+
+        // handle every uid we've been told to link with
+        $.each(data.uid, function(key, uid){
+          // break apart the uid to discover its provider and id
+          var uidParts = uid.split(':', 2);
+          var cProv = 'unknown';
+          var cId = 0;
+          if(uidParts.length === 2){
+            cProv = uidParts[0];
+            cId = uidParts[1];
+          }
+
+          // associate this account with userID
+          var cLinkAccount = $rootScope.DBFBref.child('linkedAccount/'+uid);
+          cLinkAccount.once('value',
+              function(snap){
+                var cLinkAccountData = snap.val();
+                if(!cLinkAccountData){
+                  // linkedAccount is not setup
+                  cLinkAccount.set({userID: data.userID, provider: cProv, id: cId});
+                  linkBools[uid] = true; // we'd prefer to have the uid as the index rather than value (more efficient lookups)
+                }
+                else{
+                  // linkedAccount is there...
+                  // TODO: check the userID
+                  //   if it is different from data.userID
+                  //     then this account is already linked to a different profile (throw an error?)
+                  console.log('what is linkedAccount:'+JSON.stringify(cLinkAccountData));
+                }
+              },
+              function(err){
+                console.log('error:'+err);
+              }
+          );
+        });
+
+        // ok, we can update the user profile
+        dbUserRef.child(data.userID).
+            update({displayName: data.name, testing: 'testing 123...', linkedAccounts: linkBools, created: new Date(), lastLogin: new Date()});
+      }
+    },
+    createUser: function(email, password, callback){
       auth.$createUser(email, password, true).then(
           function(user){
             if(callback){
@@ -114,6 +200,25 @@ angular.module('ecopos.common').factory('authority',function($rootScope, $fireba
 
         if(userData.uid){
           userData.notes = $rootScope.DBFB.$child('notes/'+userData.uid);
+          var userProfile = null;
+          authority.getUserByUID(userData.uid).
+              then(
+                function(userProfile){
+                  if(userProfile){
+                    console.log('win:'+JSON.stringify(userProfile));
+                    userData.userID = userProfile.userID;
+                    $rootScope.DBFB.$child('user/'+userData.userID+'/lastLogin').
+                        $set(new Date());
+                  }
+                  else{
+                    console.log('creating new user profile for uid:'+userData.uid);
+                    authority.saveUserData({userID: null, uid: userData.uid, name: userData.displayName, lastLogin: new Date()});
+                  }
+                },
+                function(err){
+                  console.log('lose:'+err);
+                }
+              );
         }
       }
 
